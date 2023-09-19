4997518502e2b45f02fd686e61d213128ee4329e
Igor Dokai Gomes <igordokai@outlook.com>
2023-09-19 12:57:13 -0300

    character

--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -3,16 +3,32 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 
-class Level extends World {
+import '../character/player.dart';
 
+class Level extends World {
+  final String levelName;
+  final Player player;
+  Level({required this.levelName, required this.player});
   late TiledComponent level;
 
   @override
   FutureOr<void> onLoad() async{
 
-    level = await TiledComponent.load('Level-01.tmx', Vector2.all(16));
+    level = await TiledComponent.load('$levelName.tmx', Vector2.all(16));
 
     add(level);
+
+    final spawnPointsLayer = level.tileMap.getLayer<ObjectGroup>("Spawnpoints");
+
+    for(final spawnPoints in spawnPointsLayer!.objects) {
+      switch (spawnPoints.class_) {
+        case "Player":
+          player.position =  Vector2(spawnPoints.x, spawnPoints.y);
+          add(player);
+          break;
+        default:
+      }
+    }
 
     return super.onLoad();
   }
--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -2,20 +2,30 @@ import 'dart:async';
 import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter_flame/character/player.dart';
 import 'package:flutter_flame/levels/level.dart';
 
-class PixelAdventure extends FlameGame {
+class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents, DragCallbacks {
   @override
   Color backgroundColor() => const Color(0xFF211F30);
   late final CameraComponent cam;
-
-  final world = Level();
+  Player player = Player(character: "Mask Dude");
+  late JoystickComponent joystick;
+  bool showJoystick = true;
 
   @override
   FutureOr<void> onLoad() async{
     //load all images into cache
     await images.loadAllImages();
+
+    final world = Level(
+      levelName: "Level-01",
+      player: player,
+    );
 
     cam = CameraComponent.withFixedResolution(
         world: world, width: 640, height: 360);
@@ -23,8 +33,55 @@ class PixelAdventure extends FlameGame {
 
     addAll([cam, world]);
 
+    if (showJoystick) {
+      addJoystick();
+    }
 
     return super.onLoad();
   }
 
+  @override
+  void update(double dt) {
+    if (showJoystick) {
+      updateJoystick();
+    }
+    super.update(dt);
+  }
+
+  void addJoystick() {
+    joystick = JoystickComponent(
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache("HUD/Knob.png"),
+        ),
+      ),
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache("HUD/Joystick.png"),
+        ),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+    );
+
+    add(joystick);
+  }
+
+  void updateJoystick() {
+    switch (joystick.direction) {
+      case JoystickDirection.left:
+      case JoystickDirection.upLeft:
+      case JoystickDirection.downLeft:
+        player.playerDirection = PlayerDirection.left;
+        break;
+      case JoystickDirection.right:
+      case JoystickDirection.upRight:
+      case JoystickDirection.downRight:
+        player.playerDirection = PlayerDirection.right;
+        break;
+      default:
+        //idle
+        player.playerDirection = PlayerDirection.none;
+        break;
+    }
+  }
 }
