f4c280c2b9d81dc26bd8faed940e2423932ee642
Igor Dokai Gomes <igordokai@outlook.com>
2023-09-24 23:58:51 -0300

    admob banner added

---
+++ lib/config/Admob/AdmobBanner.dart
@@ -0,0 +1,80 @@
+import 'dart:io';
+
+import 'package:flame/game.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_flame/pixel_adventure.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class AdmobBanner extends StatefulWidget {
+  const AdmobBanner({Key? key, required this.game}) : super(key: key);
+
+  final PixelAdventure game;
+
+  @override
+  State<StatefulWidget> createState() => _BannerExampleState();
+
+}
+
+class _BannerExampleState extends State<AdmobBanner> {
+
+  @override
+  void initState() {
+    super.initState();
+    loadAd();
+    if (_bannerAd != null) {
+
+    }
+  }
+
+  BannerAd? _bannerAd;
+  bool _isLoaded = false;
+
+  // TODO: replace this test ad unit with your own ad unit.
+  final adUnitId = Platform.isAndroid
+      ? 'ca-app-pub-3940256099942544/6300978111'
+      : 'ca-app-pub-3940256099942544/2934735716';
+
+  /// Loads a banner ad.
+  void loadAd() {
+    _bannerAd = BannerAd(
+      adUnitId: adUnitId,
+      request: const AdRequest(),
+      size: AdSize.banner,
+      listener: BannerAdListener(
+        // Called when an ad is successfully received.
+        onAdLoaded: (ad) {
+          debugPrint('$ad loaded.');
+          setState(() {
+            _isLoaded = true;
+          });
+        },
+        // Called when an ad request failed.
+        onAdFailedToLoad: (ad, err) {
+          debugPrint('BannerAd failed to load: $err');
+          // Dispose the ad here to free resources.
+          ad.dispose();
+        },
+      ),
+    )..load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          GameWidget(game: kDebugMode ? PixelAdventure() : widget.game),
+        ],
+      ),
+      bottomNavigationBar: _isLoaded ? SizedBox(
+        height: _bannerAd?.size.height.toDouble(),
+        width: _bannerAd?.size.width.toDouble(),
+        child: AdWidget(ad: _bannerAd!),
+      ) : const SizedBox(),
+    );
+    throw UnimplementedError();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,30 @@ import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_flame/config/Admob/AdmobBanner.dart';
 import 'package:flutter_flame/pixel_adventure.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   await Flame.device.fullScreen();
   await Flame.device.setLandscape();
 
   PixelAdventure game = PixelAdventure();
   runApp(
-    GameWidget(game: kDebugMode ? PixelAdventure() : game),
+    MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            GameWidget(game: kDebugMode ? PixelAdventure() : game),
+            AdmobBanner(game: game),
+          ],
+        ),
+      ),
+    )
+
   );
 }
 
